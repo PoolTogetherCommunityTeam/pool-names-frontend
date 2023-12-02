e2265984c30bb7a6110fb655871ec05d4503d9dc
Xiloror <trinity0102@live.fr>
2023-12-02 23:59:22 +0100

    css revert

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand">
-        <img src="../assets/pool-logo.svg" alt="" class="d-inline-block align-text-top navbar-img"> Names
+        <img src="../assets/pool-logo.svg" alt="" class="d-inline-block align-text-top navbar-img">
       </router-link>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
